9438ff582d6b064721443d7b785a8dc045b08dc0
Samagra <samagra8889@gmail.com>
2022-11-18 17:58:17 +0530

    added the first piece of code

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+console.log("hello world");
+console.log("i got out this in a very difficult way ");
+console.log("i am getting it slowly after getting my hands on it ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
